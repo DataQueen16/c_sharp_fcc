cf9fc5ab62d6b03986f9ea4589f0add6ff1843e7
Dataqueen16 <dataqueen16@gmail.com>
2019-12-22 21:52:20 -0600

    Working on logic, methods, referencing methods & more

--- freecodecamp_project/Program.cs
+++ freecodecamp_project/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace freecodecamp_project
 {
@@ -10,7 +6,45 @@ namespace freecodecamp_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Console.Write("Enter your name please: ");
+             string name = Console.ReadLine();
+             Console.Write("Please enter your age: ");
+             int age = Convert.ToInt32(Console.ReadLine());
+             
+             SayHi(name, age);*/
+
+            SayHi("Michelle", 23);
+            SayHi("Julian", 32);
+            SayHi("Ned", 38);
+            SayHi("Jen", 42);
+
+            int cubedNumber = Cube(5);
+
+            Console.WriteLine(cubedNumber);
+
+            bool isFemale = true;
+            bool isTall = false;
+
+            if (isFemale || isTall) 
+            {
+                Console.WriteLine("You are female and you are tall");
+            } else 
+            {
+                Console.WriteLine("You are either not female or not tall or both");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age) 
+        {
+            Console.WriteLine("Hello " + name + ", you are " + age);
+        }
+
+        static int Cube(int num)
+        {
+            int result = num * num * num;
+            return result;
         }
     }
 }
